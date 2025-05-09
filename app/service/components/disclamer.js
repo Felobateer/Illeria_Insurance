63a6651f2dc5388f5b305923e8d2ec712a157eb1
Felobateer <felobateersamy01@gmail.com>
2025-05-09 20:45:34 +0200

    add disclamer

--- app/service/components/disclamer.js
+++ app/service/components/disclamer.js
@@ -3,12 +3,10 @@ import styles from "@/app/styles";
 export default function Disclamer() {
     return (
         <section className={styles.aboutme.container}>
-            <div className={styles.aboutme.content}>
-                <h2 className={styles.aboutme.title}>Disclaimer</h2>
+            <div className={styles.aboutme.container}>
+                <h2 className={styles.about.title}>Disclaimer</h2>
                 <p className={styles.aboutme.description}>
-                    The information provided on this website is for general informational purposes only and should not be considered legal or financial advice. 
-                    We are not responsible for any errors or omissions in the content of this site. 
-                    Please consult with a qualified professional for specific advice regarding your situation.
+                Disclaimer: “We do not offer every planavailable in your area . Currently, we represent 10 organization, which offer39 products in your area. You can always contact Medicare.GOV, 1-800-Medicare,or your local State health insurance program (SHIP) to get information on allof your options.”
                 </p>
             </div>
         </section>
